c90cfea870a9ceb1804dc0bf758f542474d845b8
wjdev.iosdev.004@gmail.com <Woongjin1!>
2021-10-22 10:22:56 +0900

    sample

---
+++ sample/lib/webiew_flutter_example.screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewFlutterExampleScreen extends StatelessWidget {
+
+// String url = "https://addpipe.com/html-media-capture-demo/";
+  String url = "https://developer.mozilla.org/ko/docs/Web/HTML/Element/Input/file";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('webview flutter file tag example'),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: WebView(
+            initialUrl: url,
+            javascriptMode: JavascriptMode.unrestricted,
+            useDefaultShowFileChooser: false,
+            onPageStarted: (String url) {
+              print('Page started loading: $url');
+            },
+
+            // OnShowFileRequestResult
+            onShowFileChooser: (bool allowMultipleFiles, acceptTypes) async {
+              print('WebViewFlutterExampleScreen onShowFileChooser ===== $allowMultipleFiles   $acceptTypes');
+
+              List<String> resultUrl = ["url1", "url2"];
+              _showSnackBar(context, resultUrl.toString());
+              return resultUrl;
+            },
+          ),
+        ));
+  }
+}
+
+void _showSnackBar(BuildContext context, String msg) {
+  final scaffold = ScaffoldMessenger.of(context);
+
+  scaffold.showSnackBar(
+    SnackBar(
+      content: Text("Choose File Tag : " + msg),
+      action: SnackBarAction(label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+    ),
+  );
+}
